234e7cfdc44f364de6547f40eccfe5d093a5222a
jayprakash Kumar <jptu143@gmail.com>
2019-01-31 15:08:07 +0530

    Updates

---
+++ precache-manifest.c72a9a9ffd7f6ab2dd7079b0b4f92011.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6a71d35c1fbb3241cd8",
+    "url": "/tictactoe/static/js/runtime~main.a6a71d35.js"
+  },
+  {
+    "revision": "5a3b5b9f65754d0204fc",
+    "url": "/tictactoe/static/js/main.5a3b5b9f.chunk.js"
+  },
+  {
+    "revision": "f14bf8d25ba5fdafc7ea",
+    "url": "/tictactoe/static/js/1.f14bf8d2.chunk.js"
+  },
+  {
+    "revision": "5a3b5b9f65754d0204fc",
+    "url": "/tictactoe/static/css/main.beceb7e2.chunk.css"
+  },
+  {
+    "revision": "30d80c3688328529a2510175bda1f2f2",
+    "url": "/tictactoe/index.html"
+  }
+];
